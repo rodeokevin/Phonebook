33ad358eeb05f54a07f156fa825f9ef0dbdcf4a9
Kevin Qin <kevin.qinzw@gmail.com>
2024-08-25 23:18:43 -0400

    uibuild

---
+++ README.md
@@ -0,0 +1,1 @@
+https://phonebook-backend-injm.onrender.com/
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(express.static('dist'))
 
@@ -19,92 +20,100 @@ morgan.token('body', (req) => {
   })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-        { 
-          "id": "1",
-          "name": "Arto Hellas", 
-          "number": "040-123456"
-        },
-        { 
-          "id": "2",
-          "name": "Ada Lovelace", 
-          "number": "39-44-5323523"
-        },
-        { 
-          "id": "3",
-          "name": "Dan Abramov", 
-          "number": "12-43-234345"
-        },
-        { 
-          "id": "4",
-          "name": "Mary Poppendieck", 
-          "number": "39-23-6423122"
-        }
-    ]
-
 // GET info page of phonebook
 app.get('/info', (request, response) => {
-    const currentTime = new Date()
-    const info = `<p>Phonebook has info for ${persons.length} people</p> <p>${currentTime}</p>`
-    response.send(info)
+    Person.countDocuments({})
+      .then(count => {
+        const currentTime = new Date()
+        const info = `<p>Phonebook has info for ${count} people</p> <p>${currentTime}</p>`
+        response.send(info)
+      })
 })
 
 // GET all persons
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(people => {
+    response.json(people)
+  })
 })
 
-// GET the info of 1 person based on the id
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    person ? response.json(person) : response.status(404).end()
+// GET 1 person based on the id [ERROR HANDLER NOT DEFINED]
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      person ? response.json(person) : response.status(404).end()
+    })
+    .catch(error => next(error))
 })
 
-// DELETE a person from phonebook
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+// DELETE a person from phonebook [ERROR HANDLER NOT DEFINED]
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(person => response.status(204).end())
+    .catch(error => next(error)) // If error, pass to error handler
 })
 
 // POST a person to phonebook
 app.post('/api/persons', (request, response) => {
-    const newPerson = request.body
+  const newPerson = request.body
+  // Same name is now treated as an update
+  /*
+  if(persons.find(person => person.name.toLowerCase() === newPerson.name.toLowerCase())) {
+      return response.status(400).json({
+          error: "name must be unique"
+      })
+  }
+  */
+  if(!newPerson.name) {
+      return response.status(400).json({
+          error: "name is missing"
+      })
+  }
+  if(!newPerson.number) {
+      return response.status(400).json({
+          error: "number is missing"
+      })
+  }
+  // No errors
+  const person = new Person({
+    name: newPerson.name,
+    number: newPerson.number,
+  })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
 
-    // Error handling
-    if(persons.find(person => person.name === newPerson.name)) {
-        return response.status(400).json({
-            error: "name must be unique"
-        })
-    }
-
-    if(!newPerson.name) {
-        return response.status(400).json({
-            error: "name is missing"
-        })
-    }
-    if(!newPerson.number) {
-        return response.status(400).json({
-            error: "number is missing"
-        })
-    }
-
-    // No errors
-    const newId = Math.floor(Math.random() * 1000)
-    newPerson.id = String(newId)
-    persons = persons.concat(newPerson)
-    response.json(newPerson)
+// Update the number of an existing person
+app.put('/api/persons/:id', (request, response) => {
+  Person
+  .findByIdAndUpdate(request.params.id, request.body, {new: true})
+  .then(info => response.json(info))
 })
 
 // Middleware if requests are made to non-existant routes
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
-  }
+}
   
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+// Error handlers:
+
+// Malformatted ID handler (GET a person and DELETE a person)
+const malformattedID = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(malformattedID)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
